5824a81f8129cf4f81faf345f0b40791c94c2970
KJIHEON <youwa65@gmail.com>
2023-03-08 17:10:28 +0900

    TEST

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 
-const port = 3000
-app.use(express.json())
-app.get('/cat', (req,res)=>{
-    res.send("dddd")
-})
+// const port = 3000
+// app.use(express.json())
+// app.get('/cat', (req,res)=>{
+//     res.send("dddd")
+// })
 
-app.listen(port ,()=>{
-    console.log("포트 실행중3000")
-})
+// app.listen(port ,()=>{
+//     console.log("포트 실행중3000")
+// })
