051fcc2f730fdc003e57ececf955b9393f769163
KJIHEON <youwa65@gmail.com>
2023-03-10 11:53:11 +0900

    테스트용

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-// const express = require("express")
-// const app = express()
+import  express  from "express";
+import { Client } from "@notionhq/client";
+import dotenv from "dotenv"
+dotenv.config()
+const app = express();
+app.set("port", 3000);
+app.use(express.json())
 
-// const port = 3000
-// app.use(express.json())
-// app.get('/cat', (req,res)=>{
-//     res.send("dddd")
-// })
+app.get("/", async (req, res) => {
+  const notionKey = process.env.NOTION_KEY
+  const notionDatabaseKey = process.env.NOTION_DATABASE_KEY
 
-// app.listen(port ,()=>{
-//     console.log("포트 실행중3000")
-// })
+  const notion = new Client({ auth: notionKey })
+  const {results} = await notion.databases.query({ database_id: notionDatabaseKey })
+  const resultss = results.map((x)=>{
+    const office =  x.properties.회사명.title.map((x)=> x.plain_text).join("")
+    const name = x.properties.이름.rich_text.map((x)=>x.plain_text).join("")
+    const phone = x.properties.연락처.rich_text.map((x)=>x.plain_text).join("")
+    const content = x.properties.문의내용.rich_text.map((x)=>x.plain_text).join("")
+    const createdAt = x.properties.생성일시.created_time
+    return{
+    "회사명" : office,
+    "이름" : name,
+    "연락처" : phone,
+    "문의내용" : content,
+    "생성 일시" : createdAt,
+    }
+  })
+  console.log(resultss)
+  res.json({...resultss})
+});
+
+app.post("/:id", async (req, res) => {
+  const {id} = req.params
+  console.log(req.params)
+  res.json({id})
+});
+
+
+app.listen(app.get("port"), () => {
+  console.log("Notion API 서버 실행");
+});
